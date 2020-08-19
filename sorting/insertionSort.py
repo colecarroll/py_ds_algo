2aef319222c07e3d82f395331c17e90e0820e8fa
COLE CARROLL <colecarroll2@gmail.com>
2020-08-19 11:43:50 -0600

    added bubbleSort and insertionSort to sorting category

---
+++ sorting/insertionSort.py
@@ -0,0 +1,16 @@
+# Algorithm for insertion sort.
+
+# Best: O(n) time | O(1) space
+# Average: O(n ^ 2) time | O(1) space
+# Worst: O(n ^ 2) time | O(1) space
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j - 1] > array[j]:
+            swap(j, j - 1, array)
+            j -= 1
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
